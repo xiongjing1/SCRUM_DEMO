de67f8baa0c0b40713e81730746be1f66068fc2e
jeffiny <1223160472@qq.com>
2022-08-03 11:26:41 +0800

    团队管理1.0

--- scrum/src/App.vue
+++ scrum/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/TeamManage">TeamManage</router-link> |
+      <router-link to="/ProjectManage">ProjectManage</router-link>
     </nav>
     <router-view/>
   </div>
--- scrum/src/router/index.js
+++ scrum/src/router/index.js
@@ -17,6 +17,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/TeamManage',
+    name: 'TeamManage',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/TeamManage')
+  },
+  {
+    path: '/ProjectManage',
+    name: 'ProjectManage',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views//ProjectManage')
   }
 ]
 
