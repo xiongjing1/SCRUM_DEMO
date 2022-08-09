938cf51d27e5587a46d3f567946b97ae029dbf4a
lenovo <lluosi@hotmail.com>
2022-08-09 11:54:35 +0800

    homeview

--- scrum/src/router/index.js
+++ scrum/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import LoginView from "@/views/LoginView";
+
+import HomeView from "@/views/HomeView";
 //import HomeView from '../views/document.vue'
 
 Vue.use(VueRouter)
@@ -8,9 +9,16 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/login',
     name: 'login',
-    component: LoginView,
-
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
   },
   {
     path: '/about',
