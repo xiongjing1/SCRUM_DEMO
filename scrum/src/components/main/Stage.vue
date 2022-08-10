63b56504df317a25a92f0656e0677bfba4053d69
jeffiny <1223160472@qq.com>
2022-08-10 17:03:25 +0800

    修复部分bug

---
+++ scrum/src/components/main/Stage.vue
@@ -0,0 +1,200 @@
+<template>
+  <mr-container
+    :id="page.id"
+    :style="pageStyles"
+    :class="[page.classes, {stage: true}]"
+    @moving="movingHandler"
+    @movestop="moveStopHandler"
+
+    @clearselection="clearSelectionHandler"
+    @delete="deleteHandler"
+
+  >
+    <!-- @resizestop="resizeStopHandler"  -->
+    <!--@selectstop="selectStopHandler" -->
+    <stage-el
+      v-for="element in page.children"
+      :key="element.id"
+      :elem="element">
+    </stage-el>
+
+  </mr-container>
+</template>
+
+
+<script>
+import StageEl from './StageEl'
+import MrContainer from '@/components/common/mr-vue/MrContainer'
+import elementsFromPoint from '@/polyfills/elementsFromPoint'
+//import {getChildNode} from "@/helpers/recursiveMethods";
+
+const DROP_BORDER = {
+  width: '2px',
+  style: 'solid',
+  color: '#f1867f'
+}
+
+export default {
+  name: 'stageComp',
+  components: { StageEl, MrContainer },
+  props: ['page',],
+  data: function () {
+    return {
+      clipboard: [],
+      dropContainer: null,
+      defaultBorder: {
+        width: '',
+        style: '',
+        color: ''
+      }
+    }
+  },
+  computed: {
+    pageStyles () {
+      return {
+        ...this.page.styles,
+        height: (typeof this.page.height === 'string') ? this.page.height : (this.page.height + 'px'),
+        width: (typeof this.page.width === 'string') ? this.page.width : (this.page.width + 'px'),
+      }
+    },
+  },
+  methods: {
+    clearSelectionHandler () {
+      this.$store.commit('clear')
+    },
+
+    deleteHandler () {
+      if (this.$store.state.selectedItem.length) {
+        this.$bus.$emit('deleteElement')
+        //this.$store.commit('clear')
+      }
+    },
+    //selectStopHandler (selectionBox) {
+     // if ((selectionBox.top === selectionBox.bottom && selectionBox.left === selectionBox.right) ||
+    //      (this.page.children.length === 0)) return
+
+    //  let selectedElements = []
+    //  this.page.children.forEach(childEl => {
+    //    const child = (childEl.global) ? {...childEl, ...this.getComponentRef(childEl), id: childEl.id} : childEl
+
+    //    let childTop = getComputedProp('top', child)
+    //    let childLeft = getComputedProp('left', child)
+    //    let childBottom = getComputedProp('height', child, this.page) + childTop
+    //    let childRight = getComputedProp('width', child, this.page) + childLeft
+
+   //     if (((childTop <= selectionBox.bottom) && (childLeft <= selectionBox.right) &&
+     //           (childBottom >= selectionBox.top) && (childRight >= selectionBox.left)) ||
+    //        ((childTop <= selectionBox.bottom) && (childRight >= selectionBox.left) &&
+    //            (childBottom >= selectionBox.top) && (childLeft <= selectionBox.right))) {
+    //      selectedElements.push(child)
+    //    }
+    //  })
+//
+    //  if (selectedElements.length > 0) {
+    //    this._addSelectedElements(selectedElements)
+    //  }
+    //},
+
+    //  if (selectedElements.length > 0) {
+    //    this._addSelectedElements(selectedElements)
+    //  }
+    //},
+
+    //getComponentRef (component) {
+    //  return this.projectComponents[this.projectComponents.findIndex(comp => comp.name === component.name)]
+    //},
+
+    //resizeStopHandler (resStopData) {
+    //  resStopData.map(resElData => this.resizeElement({...resElData, pageId: this.page.id}))
+    //  this.rebaseSelectedElements()
+    //},
+
+
+    movingHandler (absMouseX, absMouseY) {
+      this.dropContainer = this.getContaineggOnPoint(absMouseX, absMouseY)
+      this.toggleDroppableCursor(!!this.dropContainer)
+    },
+
+    moveStopHandler (moveStopData) {
+      console.log('yyy')
+      console.log(moveStopData)
+      this.$bus.$emit('updateElement', moveStopData)
+      //console.log(this.$store.state.selectedItem)
+      //moveStopData.moveElData.map(moveData => this.$emit('moveElement',{
+      //  ...moveData,
+      //  pageId: this.page.id,
+      //  mouseX: moveStopData.relMouseX,
+      //  mouseY: moveStopData.relMouseY
+      //}))
+      //this.$bus.$emit('updateElement' , moveStopData)
+    },
+
+    getContaineggOnPoint (x, y) {
+      const movingEggs = this.$store.state.selectedItem
+      const parentsIds = movingEggs.id.substring(0, movingEggs.id.lastIndexOf('.'))
+      const commonParentId = parentsIds.every((val, i, arr) => val === arr[0]) ? parentsIds[0] : null
+      const elementsOnPoint = elementsFromPoint(x, y)
+
+      for (let el of elementsOnPoint) {
+        if (el.id === commonParentId) return null
+        if ((el.dataset.mrContainer) ||
+          (
+            (el.dataset.containegg) &&
+            (!el.dataset.componegg) &&
+            (movingEggs.every(egg => !el.id.includes(egg.id)))
+          )
+        ) return el
+      }
+     return null
+    },
+
+    toggleDroppableCursor (isDroppable) {
+      isDroppable
+        ? document.documentElement.classList.add('droppable')
+        : document.documentElement.classList.remove('droppable')
+    },
+
+
+  },
+  watch: {
+    dropContainer: function (newVal, oldVal) {
+      if (oldVal) {
+        oldVal.style.borderWidth = this.defaultBorder.width
+        oldVal.style.borderStyle = this.defaultBorder.style
+        oldVal.style.borderColor = this.defaultBorder.color
+      }
+
+      if (newVal) {
+        this.defaultBorder.width = newVal.style.borderWidth
+        this.defaultBorder.style = newVal.style.borderStyle
+        this.defaultBorder.color = newVal.style.borderColor
+        newVal.style.borderWidth = DROP_BORDER.width
+        newVal.style.borderStyle = DROP_BORDER.style
+        newVal.style.borderColor = DROP_BORDER.color
+      }
+    }
+  }
+}
+</script>
+
+
+<style>
+html.droppable,
+html.droppable * {
+  cursor: copy !important;
+}
+</style>
+
+<style scoped>
+.stage {
+  transform-origin: 0 0;
+  user-select: none;
+  margin: 10px auto 35px !important;
+  border: 2px solid rgba(0, 0, 0, 0);
+  /* for paper style */
+  box-shadow:
+    0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12),
+    0 3px 1px -2px rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ scrum/src/components/main/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mainegg">
+    <stageComp :page="pageselect"></stageComp>
+  </div>
+</template>
+<script>
+import StageComp from './Stage'
+export default {
+  name: 'mainEgg',
+  components: { StageComp },
+
+  props:['pageselect']
+
+}
+</script>
+
+<style scoped>
+.mainegg {
+  margin: 0 ;
+  height: 100%;
+}
+</style>
